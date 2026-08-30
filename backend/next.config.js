5a6318ca6df3dd486d6078e0a9eab8d0f1b1a6bd
muhammad962-fiv <muhammad.ahmad00962@gmail.com>
2026-08-30 20:43:51 +0500

    config files

---
+++ backend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: { serverActions: true },
+};
+module.exports = nextConfig;
